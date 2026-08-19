0c1d6d522732610e7989ce9e1d3e0916a2e4f7ca
mave3188 <mave3188@gmail.com>
2026-08-19 19:07:01 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+set -e
+
+pkg update -y
+pkg upgrade -y
+
+pkg install git python nodejs make -y
+
+python -m pip install --upgrade pip
+python -m pip install pycryptodome
+
+git clone https://github.com/mave3188/butterfly-
+
+cd "$HOME/butterfly-"
+
+npm install -g @whiskeysockets/baileys pino qrcode-terminal
+
+make run
